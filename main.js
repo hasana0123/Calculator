286f79cb82e783681ff2e5f25f13f556ab16187f
Bibek Manandhar <manandhar.bibek01@gmail.com>
2021-06-10 19:13:11 +0545

    Brackets implemented

--- main.js
+++ main.js
@@ -29,6 +29,10 @@ const multiplication = document.querySelector("#multiply");
 const division = document.querySelector("#divide");
 const arithmeticKeys = [addition, subtraction, multiplication, division];
 
+const openingBracket = document.querySelector("#opening-bracket");
+const closingBracket = document.querySelector("#closing-bracket");
+const brackets = [openingBracket, closingBracket];
+
 const equalsTo = document.querySelector("#equalsto");
 const del = document.querySelector("#delete");
 const allClear = document.querySelector("#allclear");
@@ -41,6 +45,7 @@ var operators = {};
 var operands = {};
 var allExpressions = "";
 var refinedExpression = "";
+var expressionInsideBracket = "";
 
 numbersKeys.forEach((key) => {
     key.addEventListener("click", () => {
@@ -66,6 +71,15 @@ arithmeticKeys.forEach((key) => {
     });
 });
 
+brackets.forEach((bracket) => {
+    bracket.addEventListener("click", () => {
+        const Bracket = bracket.textContent;
+        expression.removeChild(cursor);
+        expression.textContent += Bracket;
+        expression.appendChild(cursor);
+    });
+});
+
 del.addEventListener("click", () => {
     expression.removeChild(cursor);
     expression.textContent = expression.textContent.slice(0, -1);
@@ -82,18 +96,40 @@ allClear.addEventListener("click", () => {
 });
 
 equalsTo.addEventListener("click", () => {
-    expression.removeChild(cursor);
+    if ([...expression.children].includes(cursor)) {
+        expression.removeChild(cursor);
+    }
     allExpressions = expression.textContent.trim();
-    if(allExpressions.length) {
+    if (allExpressions.length) {
         const isValid = verify(allExpressions);
         if (isValid) {
             console.log("the expression is valid");
-            let result = evaluate(refinedExpression);
+            console.log(refinedExpression);
+            let exp = refinedExpression;
+            while (checkForBrackets(exp)) {
+                let index = exp.indexOf(expressionInsideBracket);
+                if (
+                    exp.length >
+                    exp.indexOf(expressionInsideBracket) +
+                        expressionInsideBracket.length
+                ) {
+                    exp = exp.replace(
+                        `(${expressionInsideBracket})`,
+                        evaluate(expressionInsideBracket)
+                    );
+                } else {
+                    exp = exp.replace(
+                        `(${expressionInsideBracket}`,
+                        evaluate(expressionInsideBracket)
+                    );
+                }
+            }
+            let result = evaluate(exp);
             console.log("result", result);
             evaluation.textContent = result;
         } else {
             console.log("the expression is not valid");
-            evaluation.textContent = 'Syntax Error';
+            evaluation.textContent = "Syntax Error";
         }
     }
 });
@@ -101,17 +137,23 @@ equalsTo.addEventListener("click", () => {
 const verify = (exp) => {
     let isValid = true;
     let isComplete = false;
+    let openingBracketCount = 0;
+    let closingBracketCount = 0;
     let refinedExp = exp.charAt(0);
     let previousCharacter = exp.charAt(0);
-    if(refinedExp == '.') {
-        refinedExp = '0.';
+    if (refinedExp == ".") {
+        refinedExp = "0.";
+    } else if (refinedExp == ")") {
+        isValid = false;
+        isComplete = true;
+    } else if (refinedExp == "(") {
+        openingBracketCount++;
     }
-    if(exp.length == 1) {
-        if(isNaN(exp)) {
+    if (exp.length == 1) {
+        if (isNaN(exp)) {
             isValid = false;
         }
-    }
-    else {
+    } else {
         while (true) {
             if (isComplete) {
                 break;
@@ -120,8 +162,27 @@ const verify = (exp) => {
                 let currentCharacter = exp.charAt(i);
                 if (i == exp.length - 1) {
                     if (isNaN(currentCharacter)) {
-                        isValid = false;
-                        isComplete = true;
+                        if (currentCharacter == ")") {
+                            closingBracketCount++;
+                            if (closingBracketCount > openingBracketCount) {
+                                isValid = false;
+                                isComplete = true;
+                            } else {
+                                if (
+                                    !isNaN(previousCharacter) ||
+                                    previousCharacter == ")"
+                                ) {
+                                    refinedExp += currentCharacter;
+                                    isComplete = true;
+                                } else {
+                                    isValid = false;
+                                    isComplete = true;
+                                }
+                            }
+                        } else {
+                            isValid = false;
+                            isComplete = true;
+                        }
                     } else {
                         refinedExp += currentCharacter;
                         isComplete = true;
@@ -135,21 +196,65 @@ const verify = (exp) => {
                                     isComplete = true;
                                     break;
                                 } else {
-                                    refinedExp += `0${currentCharacter}`;
-                                    previousCharacter = currentCharacter;
+                                    if (previousCharacter == ")") {
+                                        refinedExp += `x0${currentCharacter}`;
+                                        previousCharacter = currentCharacter;
+                                    } else {
+                                        refinedExp += `0${currentCharacter}`;
+                                        previousCharacter = currentCharacter;
+                                    }
+                                }
+                            } else if (currentCharacter == "(") {
+                                openingBracketCount++;
+                                if (previousCharacter == ".") {
+                                    isValid = false;
+                                    isComplete = true;
+                                    break;
+                                } else {
+                                    if (previousCharacter == ")") {
+                                        refinedExp += `x${currentCharacter}`;
+                                        previousCharacter = currentCharacter;
+                                    } else {
+                                        refinedExp += currentCharacter;
+                                        previousCharacter = currentCharacter;
+                                    }
+                                }
+                            } else if (currentCharacter == ")") {
+                                closingBracketCount++;
+                                if (closingBracketCount > openingBracketCount) {
+                                    isValid = false;
+                                    isComplete = true;
+                                    break;
+                                } else {
+                                    if (previousCharacter == ")") {
+                                        refinedExp += currentCharacter;
+                                        previousCharacter = currentCharacter;
+                                    } else {
+                                        isValid = false;
+                                        isComplete = true;
+                                        break;
+                                    }
                                 }
                             } else if (
                                 currentCharacter == "/" ||
                                 currentCharacter == "x"
                             ) {
-                                isValid = false;
-                                isComplete = true;
-                                break;
+                                if (previousCharacter == ")") {
+                                    refinedExp += currentCharacter;
+                                    previousCharacter = currentCharacter;
+                                } else {
+                                    isValid = false;
+                                    isComplete = true;
+                                    break;
+                                }
                             } else if (currentCharacter == "+") {
                                 if (previousCharacter == ".") {
                                     isValid = false;
                                     isComplete = true;
                                     break;
+                                } else if (previousCharacter == ")") {
+                                    refinedExp += currentCharacter;
+                                    previousCharacter = currentCharacter;
                                 }
                             } else {
                                 if (previousCharacter == ".") {
@@ -159,7 +264,9 @@ const verify = (exp) => {
                                 } else {
                                     if (
                                         previousCharacter == "/" ||
-                                        previousCharacter == "x"
+                                        previousCharacter == "x" ||
+                                        previousCharacter == "(" ||
+                                        previousCharacter == ")"
                                     ) {
                                         refinedExp += currentCharacter;
                                         previousCharacter = currentCharacter;
@@ -169,29 +276,100 @@ const verify = (exp) => {
                                             currentCharacter;
                                         previousCharacter = currentCharacter;
                                     } else {
-                                        refinedExp = refinedExp.slice(0, -1) + "+";
+                                        refinedExp =
+                                            refinedExp.slice(0, -1) + "+";
                                         previousCharacter = "+";
                                     }
                                 }
                             }
                         } else {
+                            if (currentCharacter == "(") {
+                                openingBracketCount++;
+                                refinedExp += `x${currentCharacter}`;
+                                previousCharacter = currentCharacter;
+                            } else if (currentCharacter == ")") {
+                                closingBracketCount++;
+                                if (closingBracketCount > openingBracketCount) {
+                                    isValid = false;
+                                    isComplete = true;
+                                    break;
+                                } else {
+                                    refinedExp += currentCharacter;
+                                    previousCharacter = currentCharacter;
+                                }
+                            } else {
+                                refinedExp += currentCharacter;
+                                previousCharacter = currentCharacter;
+                            }
+                        }
+                    } else {
+                        if (previousCharacter == ")") {
+                            refinedExp += `x${currentCharacter}`;
+                            previousCharacter = currentCharacter;
+                        } else {
                             refinedExp += currentCharacter;
                             previousCharacter = currentCharacter;
                         }
-                    } else {
-                        refinedExp += currentCharacter;
-                        previousCharacter = currentCharacter;
                     }
                 }
             }
         }
     }
+    console.log(openingBracketCount, closingBracketCount);
     if (isValid) {
         refinedExpression = refinedExp;
         return true;
     } else {
         return false;
     }
+};
+
+const checkForBrackets = (exp) => {
+    let startIndex;
+    let endIndex;
+    for (let i = 0; i < exp.length; i++) {
+        if (exp.charAt(i) == "(") {
+            startIndex = i;
+        } else if (exp.charAt(i) == ")") {
+            endIndex = i;
+            break;
+        }
+    }
+    if (!isNaN(startIndex)) {
+        if (!isNaN(endIndex)) {
+            expressionInsideBracket = exp.slice(startIndex + 1, endIndex);
+        } else {
+            expressionInsideBracket = exp.slice(startIndex + 1);
+        }
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const evaluate = (Expression) => {
+    if (countOperators(Expression)) {
+        while (countOperators(Expression, "/")) {
+            console.log("division");
+            setOperands(Expression, Expression.indexOf("/"));
+            Expression = Expression.replace(
+                `${operands.first}/${operands.second}`,
+                operate("/")
+            );
+        }
+        while (countOperators(Expression, "x")) {
+            console.log("multiplication");
+            setOperands(Expression, Expression.indexOf("x"));
+            console.log(operands.first);
+            console.log(operands.second);
+            Expression = Expression.replace(
+                `${operands.first}x${operands.second}`,
+                operate("x")
+            );
+        }
+        Expression = sumUp(Expression);
+    }
+    return Expression;
 };
 
 const countOperators = (exp, operator = "all") => {
@@ -303,7 +481,7 @@ const sumUp = (exp) => {
     let isNegative = false;
     let j = 0;
     let flag = false;
-    if(countOperators(exp)) {
+    if (countOperators(exp)) {
         if (countOperators(exp) == 1) {
             if (isNaN(exp.charAt(0))) {
                 return Number(Math.round(Number(exp) + "e10") + "e-10");
@@ -340,8 +518,9 @@ const sumUp = (exp) => {
                         continue;
                     }
                     number = Number(
-                        Math.round(number * 10 + Number(exp.charAt(i)) + "e10") +
-                            "e-10"
+                        Math.round(
+                            number * 10 + Number(exp.charAt(i)) + "e10"
+                        ) + "e-10"
                     );
                 }
             }
@@ -351,36 +530,10 @@ const sumUp = (exp) => {
         } else {
             sum = Number(Math.round(sum + number + "e10") + "e-10");
         }
-    }
-    else {
+    } else {
         sum = Number(Math.round(Number(exp) + "e10") + "e-10");
     }
     return sum;
-};
-
-const evaluate = (Expression) => {
-    countOperators(Expression);
-
-    while (countOperators(Expression, "/")) {
-        console.log('division');
-        setOperands(Expression, Expression.indexOf("/"));
-        Expression = Expression.replace(
-            `${operands.first}/${operands.second}`,
-            operate("/")
-        );
-    }
-    while (countOperators(Expression, "x")) {
-        console.log('multiplication');
-        setOperands(Expression, Expression.indexOf("x"));
-        console.log(operands.first);
-        console.log(operands.second);
-        Expression = Expression.replace(
-            `${operands.first}x${operands.second}`,
-            operate("x")
-        );
-    }
-    Expression = sumUp(Expression);
-    return Expression;
 };
 
 var blink = setInterval(handleBlink, 500);
